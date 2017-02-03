9ff8b4ff7a98edb14723e3ad434646870a401e27
Matej Cotman <cotman.matej@gmail.com>
2017-02-03 18:05:50 +0100

    add advanced example

--- app.js
+++ app.js
@@ -1,23 +1,76 @@
 const express = require('express');
 const app = express();
-var graphqlHTTP = require('express-graphql');
-var graphql = require('graphql');
-var graphQLSchema = require('./lib');
+const graphqlHTTP = require('express-graphql');
+const graphQLSchema = require('./lib');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLID
+} = require('graphql');
 
-graphQLSchema('./test/fixtures/petstore.json').then(schema => {
-  app.use('/graphql', graphqlHTTP(() => {
+let extraQueries = (queries) => {
     return {
-      schema,
-      context: {
-        GQLProxyBaseUrl: 'http://petstore.swagger.io/v2'
-      },
-      graphiql: true
+        getPet: {
+            type: new GraphQLObjectType({
+                name: 'PetOut',
+                fields: {
+                    name: {
+                        type: GraphQLString
+                    },
+                    id: {
+                        type: GraphQLID
+                    }
+                }
+            }),
+            args: {
+                id: {
+                    type: GraphQLID
+                }
+            },
+            resolve(_, args, opts) {
+                return queries.getPetById.resolve(null, {
+                    petId: String(args.id)
+                }, opts);
+            }
+        }
     };
-  }));
+};
 
-  app.listen(3009, 'localhost', () => {
-    console.info(`http://localhost:3009/graphql`);
-  });
+let extraMutations = (queries, mutations) => {
+    return {
+        doPetOrder: {
+            type: queries.getPet.type,
+            args: queries.getPet.args,
+            resolve(a, args, opts) {
+                return mutations.placeOrder.resolve(null, {body: { petId: args.id }}, opts)
+                    .then(res => {
+                        return queries.getPet.resolve(null, {
+                            id: String(res.petId)
+                        }, opts);
+                    });
+            }
+        }
+    };
+};
+
+
+graphQLSchema('./test/fixtures/petstore.json', {
+    extraQueries,
+    extraMutations
+}).then(schema => {
+    app.use('/graphql', graphqlHTTP(() => {
+        return {
+            schema,
+            context: {
+                GQLProxyBaseUrl: 'http://petstore.swagger.io/v2'
+            },
+            graphiql: true
+        };
+    }));
+
+    app.listen(3009, 'localhost', () => {
+        console.info(`http://localhost:3009/graphql`);
+    });
 }).catch(e => {
-  console.log(e);
+    console.log(e);
 });
